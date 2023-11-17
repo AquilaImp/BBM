c5f8091a324c0b9f9a3535c3bf2125fc79258063
AquilaImp <115782835+AquilaImp@users.noreply.github.com>
2023-11-17 12:27:12 +0500

    Update Program.cs

--- 2lb/NamedPipeClient/Program.cs
+++ 2lb/NamedPipeClient/Program.cs
@@ -1,135 +1,73 @@
 using System;
-using System.Collections.Generic;
 using System.IO.Pipes;
-using System.Runtime.InteropServices;
-using System.Threading;
 using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
-class Program
+namespace Client
 {
-    public struct Ad
+    class Program
     {
-        public int X;
-        public int Y;
-        public bool DA;
-        public override string ToString() => $"Данные = {X}, Ответ = {DA}";
-    }
-
-    static readonly Queue<Ad> dataQueue = new Queue<Ad>();
-    static readonly Queue<Ad> highPriorityQueue = new Queue<Ad>();
-    static readonly object dataQueueLock = new object();
-    static readonly object bufferLock = new object();
-    static CancellationTokenSource cts = new CancellationTokenSource();
-    static List<Ad> dataBuffer = new List<Ad>();
-
-    static async Task Main()
-    {
-        Console.WriteLine("Ожидание клиента...\n");
-
-        using (var serverPipe = new NamedPipeServerStream("tonel", PipeDirection.InOut, NamedPipeServerStream.MaxAllowedServerInstances, PipeTransmissionMode.Byte, PipeOptions.Asynchronous))
+        public struct Ad
         {
-            // Запускаем задачу для асинхронного чтения данных
-            var readingTask = ReadDataAsync(serverPipe);
-
-            // Запускаем задачу для обработки данных и отправки ответа
-            var processingTask = ProcessDataAsync(serverPipe);
-
-            // Запускаем задачу для обработки Ctrl+C
-            var ctrlCTask = HandleCtrlCAsync();
-
-            // Ждем, пока пользователь нажмет Ctrl+C
-            Console.WriteLine("Нажмите Ctrl+C для завершения.");
-            Console.CancelKeyPress += (s, e) =>
-            {
-                cts.Cancel();
-                e.Cancel = true; // Предотвратить закрытие консоли
-            };
-            cts.Token.WaitHandle.WaitOne();
-
-            // Ожидаем завершения задач чтения, обработки данных и Ctrl+C
-            await Task.WhenAll(readingTask, processingTask, ctrlCTask);
-
-            // Выводим буфер данных на экран или записываем в файл
-            lock (bufferLock)
-            {
-                foreach (var item in dataBuffer)
-                {
-                    Console.WriteLine($"Сохраненные данные: X={item.X}, Y={item.Y}, DA={item.DA}");
-                }
-            }
+            public int X;
+            public int Y;
+            public bool DA;
         }
 
-        Console.WriteLine("Сервер завершил работу.");
-    }
-
-    static async Task ReadDataAsync(NamedPipeServerStream pipe)
-    {
-        while (true)
+        static async Task Main()
         {
-            var buffer = new byte[Marshal.SizeOf<Ad>()];
-            await pipe.ReadAsync(buffer, 0, buffer.Length);
-            var receivedData = MemoryMarshal.Read<Ad>(buffer);
+            Console.WriteLine("Соединяю с сервером...\n");
 
-            // Добавляем данные в очередь или очередь с высоким приоритетом
-            if (receivedData.DA)
+            using (var clientPipe = new NamedPipeClientStream(".", "tonel", PipeDirection.InOut, PipeOptions.Asynchronous))
             {
-                lock (dataQueueLock)
-                {
-                    highPriorityQueue.Enqueue(receivedData);
-                }
-            }
-            else
-            {
-                lock (dataQueueLock)
-                {
-                    dataQueue.Enqueue(receivedData);
-                }
-            }
-        }
-    }
+                await clientPipe.ConnectAsync();
 
-    static async Task ProcessDataAsync(NamedPipeServerStream pipe)
-    {
-        while (true)
-        {
-            Ad dataToSend;
+                Console.WriteLine("Соединение установлено, отправляем данные...\n");
 
-            // Извлекаем данные из очереди с высоким приоритетом, если она не пуста
-            lock (dataQueueLock)
-            {
-                if (highPriorityQueue.Count > 0)
+                Ad dataToSend = new Ad
                 {
-                    dataToSend = highPriorityQueue.Dequeue();
+                    X = 42,
+                    Y = 24,
+                    DA = false
+                };
+
+                Console.WriteLine($"Отправлены данные: X={dataToSend.X}, Y={dataToSend.Y}, DA={dataToSend.DA}\n");
+
+                // Отправляем данные на сервер
+                byte[] buffer = new byte[Marshal.SizeOf<Ad>()];
+                MemoryMarshal.Write(buffer, ref dataToSend);
+                await clientPipe.WriteAsync(buffer, 0, buffer.Length);
+
+                // Ждем ответ от сервера
+                buffer = new byte[Marshal.SizeOf<Ad>()];
+                await clientPipe.ReadAsync(buffer, 0, buffer.Length);
+
+                Ad receivedData = MemoryMarshal.Read<Ad>(buffer);
+
+                Console.WriteLine($"Получены данные от сервера: X={receivedData.X}, Y={receivedData.Y}, DA={receivedData.DA}\n");
+
+                // Сохраняем полученные данные в файл или выводим на экран
+                Console.WriteLine("Выберите действие: \n1. Вывести на экран\n2. Сохранить в файл");
+                var choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    Console.WriteLine($"Полученные данные: X={receivedData.X}, Y={receivedData.Y}, DA={receivedData.DA}\n");
                 }
-                else if (dataQueue.Count > 0)
+                else if (choice == "2")
                 {
-                    dataToSend = dataQueue.Dequeue();
+                    Console.Write("Введите имя файла для сохранения: ");
+                    var fileName = Console.ReadLine();
+                    File.WriteAllText(fileName, $"Полученные данные: X={receivedData.X}, Y={receivedData.Y}, DA={receivedData.DA}\n");
+                    Console.WriteLine($"Данные сохранены в файле: {fileName}\n");
                 }
                 else
                 {
-                    continue; // Ожидаем, если обе очереди пусты
+                    Console.WriteLine("Некорректный выбор.");
                 }
             }
 
-            dataToSend.X += dataToSend.Y;
-            dataToSend.Y -= dataToSend.X;
-            dataToSend.DA = true;
-
-            // Сохраняем данные в буфер
-            lock (bufferLock)
-            {
-                dataBuffer.Add(dataToSend);
-            }
-
-            // Отправляем данные клиенту
-            var buffer = new byte[Marshal.SizeOf<Ad>()];
-            MemoryMarshal.Write(buffer, ref dataToSend);
-            await pipe.WriteAsync(buffer, 0, buffer.Length);
+            Console.WriteLine("Клиент завершил работу.");
         }
-    }
-
-    static async Task HandleCtrlCAsync()
-    {
-        await Task.Delay(-1, cts.Token); // Ожидание нажатия Ctrl+C
     }
 }
