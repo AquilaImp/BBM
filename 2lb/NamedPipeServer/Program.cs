448871c3c859efc31b58fde800415f8f51039754
AquilaImp <115782835+AquilaImp@users.noreply.github.com>
2023-12-14 14:58:15 +0500

    Update Program.cs

--- 2lb/NamedPipeServer/Program.cs
+++ 2lb/NamedPipeServer/Program.cs
@@ -1,124 +1,85 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Pipes;
+using System.Text;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 
+public struct UserInfo
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public UserInfo(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
+
 class Program
 {
-    public struct Ad
-    {
-        public int X;
-        public int Y;
-        public bool DA;
-    }
+    static Queue<UserInfo> dataQueue = new Queue<UserInfo>();
+    static CancellationTokenSource cts = new CancellationTokenSource();
 
     static async Task Main(string[] args)
     {
-        Console.CancelKeyPress += (sender, eventArgs) => { eventArgs.Cancel = true; };
-
-        CancellationTokenSource cts = new CancellationTokenSource();
-        var token = cts.Token;
-
-        // Очередь для отправки данных
-        var dataQueue = new PriorityQueue<DataItem>();
-
-        // Буфер для сохранения полученных данных
-        var dataBuffer = new List<Ad>();
-
-        // Запуск потока для обработки данных в очереди
-        var processingThread = new Thread(() =>
+        using (NamedPipeServerStream pipeServer = new NamedPipeServerStream("MyPipe", PipeDirection.InOut))
         {
-            while (!token.IsCancellationRequested)
+            Console.WriteLine("Ожидание подключения клиента...");
+            await pipeServer.WaitForConnectionAsync(cts.Token);
+
+            // Запуск асинхронного чтения из очереди и обработки данных
+            Task processQueueTask = ProcessQueueAsync();
+
+            while (true)
             {
-                if (dataQueue.TryDequeue(out var dataItem))
+                try
                 {
-                    // Обработка данных
-                    Console.WriteLine($"Processing data: X={dataItem.Data.X}, Y={dataItem.Data.Y}, DA={dataItem.Data.DA}, Priority: {dataItem.Priority}");
-                    dataBuffer.Add(dataItem.Data);
+                    byte[] buffer = new byte[1024];
+                    int bytesRead = await pipeServer.ReadAsync(buffer, 0, buffer.Length, cts.Token);
+
+                    if (bytesRead > 0)
+                    {
+                        string jsonUserData = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                        UserInfo user = JsonSerializer.Deserialize<UserInfo>(jsonUserData);
+
+                        // Добавление данных в очередь с учетом приоритета
+                        dataQueue.Enqueue(user);
+
+                        // Отправка ответа клиенту
+                        string response = $"Данные приняты: Имя: {user.Name}, Возраст: {user.Age}";
+                        byte[] responseBuffer = Encoding.UTF8.GetBytes(response);
+                        await pipeServer.WriteAsync(responseBuffer, 0, responseBuffer.Length, cts.Token);
+                    }
                 }
-                else
+                catch (IOException)
                 {
-                    // Ждем некоторое время, чтобы не нагружать процессор
-                    Thread.Sleep(100);
+                    // Обработка разрыва соединения
+                    break;
                 }
             }
-        });
-        processingThread.Start();
 
-        // Добавление данных в очередь
-        Console.WriteLine("Server is running. Press Ctrl+C to stop.");
-        while (!token.IsCancellationRequested)
+            // Ожидание завершения обработки очереди перед закрытием
+            await processQueueTask;
+        }
+    }
+
+    static async Task ProcessQueueAsync()
+    {
+        while (!cts.Token.IsCancellationRequested)
         {
-            // Ожидаем подключение клиента
-            using (var serverPipe = new NamedPipeServerStream("tonel", PipeDirection.InOut, 1, PipeTransmissionMode.Byte, PipeOptions.Asynchronous))
+            if (dataQueue.Count > 0)
             {
-                await serverPipe.WaitForConnectionAsync(token);
-
-                // Принимаем данные от клиента
-                byte[] buffer = new byte[Marshal.SizeOf<Ad>()];
-                await serverPipe.ReadAsync(buffer, 0, buffer.Length);
-
-                Ad receivedData = MemoryMarshal.Read<Ad>(buffer);
-
-                Console.WriteLine($"Received data from client: X={receivedData.X}, Y={receivedData.Y}, DA={receivedData.DA}");
-
-                // Добавляем данные в очередь с приоритетом
-                Console.Write("Enter priority for the received data: ");
-                if (int.TryParse(Console.ReadLine(), out int priority))
-                {
-                    dataQueue.Enqueue(new DataItem { Data = receivedData, Priority = priority });
-                }
-                else
-                {
-                    Console.WriteLine("Invalid priority value. Please enter an integer.");
-                }
+                UserInfo user = dataQueue.Dequeue();
+                Console.WriteLine($"Обработка данных из очереди: Имя: {user.Name}, Возраст: {user.Age}");
             }
+
+            // Добавьте здесь нужные операции с данными, например, запись в файл
+
+            await Task.Delay(100); // Имитация обработки данных
         }
-
-        // Ожидание завершения работы сервера при нажатии Ctrl+C
-        Console.WriteLine("Press Ctrl+C to stop the server.");
-        Console.CancelKeyPress += (s, e) =>
-        {
-            e.Cancel = true; // отменяем завершение приложения
-            cts.Cancel(); // отменяем выполнение задачи
-        };
-        await Task.Delay(-1); // ждем, пока не будет нажата Ctrl+C
-    }
-}
-
-// Класс для представления данных с приоритетом
-class DataItem : IComparable<DataItem>
-{
-    public Ad Data { get; set; }
-    public int Priority { get; set; }
-
-    public int CompareTo(DataItem other)
-    {
-        // Сравнение по приоритету (меньший приоритет - выше)
-        return Priority.CompareTo(other.Priority);
-    }
-}
-
-// Очередь с приоритетами
-class PriorityQueue<T> where T : IComparable<T>
-{
-    private readonly SortedSet<T> _set = new SortedSet<T>();
-
-    public void Enqueue(T item)
-    {
-        _set.Add(item);
-    }
-
-    public bool TryDequeue(out T item)
-    {
-        if (_set.Count > 0)
-        {
-            item = _set.Min;
-            _set.Remove(item);
-            return true;
-        }
-
-        item = default;
-        return false;
     }
 }
